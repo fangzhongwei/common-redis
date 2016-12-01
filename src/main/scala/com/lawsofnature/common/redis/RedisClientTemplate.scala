f1357f414fd82418abecf851965992377cfd2197
fangzhongwei <zhongwei.fang@56qq.com>
2016-12-01 16:57:14 +0800

    sync

--- src/main/scala/com/lawsofnature/common/redis/RedisClientTemplate.scala
+++ src/main/scala/com/lawsofnature/common/redis/RedisClientTemplate.scala
@@ -8,9 +8,6 @@ import com.lawsofnature.common.helper.JsonHelper
 import org.slf4j.LoggerFactory
 import redis.clients.jedis._
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Future, Promise}
-
 /**
   * Created by fangzhongwei on 2016/11/23.
   */
@@ -19,15 +16,15 @@ trait RedisClientTemplate {
 
   def close
 
-  def setString(key: String, value: String, expireSeconds: Int): Future[Option[Boolean]]
+  def setString(key: String, value: String, expireSeconds: Int): Boolean
 
-  def set(key: String, value: AnyRef, expireSeconds: Int): Future[Option[Boolean]]
+  def set(key: String, value: AnyRef, expireSeconds: Int): Boolean
 
-  def get[T](key: String, c: Class[T]): Future[Option[T]]
+  def get[T](key: String, c: Class[T]): Option[T]
 
-  def getString(key: String): Future[Option[String]]
+  def getString(key: String): Option[String]
 
-  def delete(key: String): Future[Option[Boolean]]
+  def delete(key: String): Boolean
 }
 
 class RedisClientTemplateImpl @Inject()(@Named("redis.shards") cluster: String,
@@ -75,53 +72,43 @@ class RedisClientTemplateImpl @Inject()(@Named("redis.shards") cluster: String,
 
   override def close: Unit = shardedJedisPool.close()
 
-  override def set(key: String, value: AnyRef, expireSeconds: Int): Future[Option[Boolean]] = setString(key, JsonHelper.writeValueAsString(value), expireSeconds)
+  override def set(key: String, value: AnyRef, expireSeconds: Int): Boolean = setString(key, JsonHelper.writeValueAsString(value), expireSeconds)
 
-  override def setString(key: String, value: String, expireSeconds: Int): Future[Option[Boolean]] = {
+  override def setString(key: String, value: String, expireSeconds: Int): Boolean = {
     var shardedJedis: ShardedJedis = null
-    val promise: Promise[Option[Boolean]] = Promise[Option[Boolean]]()
     try {
-      Future {
-        assert(expireSeconds > 0, "expect expireSeconds > 0")
-        val keyBytes: Array[Byte] = key.getBytes(CHARSET)
-        val valueBytes: Array[Byte] = value.getBytes(CHARSET)
-        assert(keyBytes.length < MAX_KEY_BYTES, "expect keyBytes < " + MAX_KEY_BYTES)
-        assert(valueBytes.length < MAX_VALUE_BYTES, "expect valueBytes < " + MAX_VALUE_BYTES)
-        shardedJedis = getShardedJedis
-        val pipel: ShardedJedisPipeline = shardedJedis.pipelined()
-        val response: Response[String] = pipel.set(keyBytes, valueBytes)
-        pipel.expire(keyBytes, expireSeconds)
-        pipel.sync()
-        promise.success(Some(SUCCESS_TAG.equals(response.get())))
-      }
+      assert(expireSeconds > 0, "expect expireSeconds > 0")
+      val keyBytes: Array[Byte] = key.getBytes(CHARSET)
+      val valueBytes: Array[Byte] = value.getBytes(CHARSET)
+      assert(keyBytes.length < MAX_KEY_BYTES, "expect keyBytes < " + MAX_KEY_BYTES)
+      assert(valueBytes.length < MAX_VALUE_BYTES, "expect valueBytes < " + MAX_VALUE_BYTES)
+      shardedJedis = getShardedJedis
+      val pipel: ShardedJedisPipeline = shardedJedis.pipelined()
+      val response: Response[String] = pipel.set(keyBytes, valueBytes)
+      pipel.expire(keyBytes, expireSeconds)
+      pipel.sync()
+      true
     } catch {
-      case ex: Exception => promise.failure(ex)
+      case ex: Exception =>
+        logger.error("cache", ex)
+        false
     } finally {
       if (shardedJedis != null) shardedJedis.close()
     }
-    promise.future
   }
 
-  override def get[T](key: String, c: Class[T]): Future[Option[T]] = {
-    val promise: Promise[Option[T]] = Promise[Option[T]]()
-    Future {
-      getStringFromCache(key) match {
-        case Some(str) => Some(JsonHelper.read[T](str, c))
-        case None => promise.success(None)
-      }
+  override def get[T](key: String, c: Class[T]): Option[T] = {
+    getStringFromCache(key) match {
+      case Some(str) => Some(JsonHelper.readByType[T](str, c))
+      case None => None
     }
-    promise.future
   }
 
-  override def getString(key: String): Future[Option[String]] = {
-    val promise: Promise[Option[String]] = Promise[Option[String]]()
-    Future {
-      getStringFromCache(key) match {
-        case Some(str) => promise.success(Some(str))
-        case None => promise.success(None)
-      }
+  override def getString(key: String): Option[String] = {
+    getStringFromCache(key) match {
+      case Some(str) => Some(str)
+      case None => None
     }
-    promise.future
   }
 
   def getStringFromCache(key: String): Option[String] = {
@@ -132,31 +119,26 @@ class RedisClientTemplateImpl @Inject()(@Named("redis.shards") cluster: String,
       val response: Response[Array[Byte]] = pipel.get(key.getBytes(CHARSET))
       pipel.sync()
       val bytes: Array[Byte] = response.get()
-      bytes.length match {
-        case 0 => None
-        case _ => Some(new String(bytes, CHARSET))
-      }
+      if (bytes == null || bytes.length == 0) None else Some(new String(bytes, CHARSET))
     } finally {
       if (shardedJedis != null) shardedJedis.close()
     }
   }
 
-  override def delete(key: String): Future[Option[Boolean]] = {
+  override def delete(key: String): Boolean = {
     var shardedJedis: ShardedJedis = null
-    val promise: Promise[Option[Boolean]] = Promise[Option[Boolean]]()
     try {
-      Future {
-        shardedJedis = getShardedJedis
-        val pipel: ShardedJedisPipeline = shardedJedis.pipelined()
-        val response: Response[Long] = pipel.del(key.getBytes(CHARSET))
-        pipel.sync()
-        promise.success(Some(response.get() == 1))
-      }
+      shardedJedis = getShardedJedis
+      val pipel: ShardedJedisPipeline = shardedJedis.pipelined()
+      val response: Response[Long] = pipel.del(key.getBytes(CHARSET))
+      pipel.sync()
+      response.get() == 1
     } catch {
-      case ex: Exception => promise.failure(ex)
+      case ex: Exception =>
+        logger.error("cache", ex)
+        false
     } finally {
       if (shardedJedis != null) shardedJedis.close()
     }
-    promise.future
   }
 }
